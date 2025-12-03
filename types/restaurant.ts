e06d0d13975aa86ab62930aba3c0b896a9c53b26
karinfdez <karinfdez@yahoo.com>
2025-12-02 22:40:53 -0800

    Add types folder

---
+++ types/restaurant.ts
@@ -0,0 +1,11 @@
+
+export type Restaurant = {
+    id: string
+    name: string
+    type: string
+    image: string
+    location: string
+    rating: number
+    price: string
+    description?: string
+  }
