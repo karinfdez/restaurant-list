87cc799a5d92ceea476f4f29706d33635765b287
karinfdez <karinfdez@yahoo.com>
2025-12-03 10:17:59 -0800

    Add list of mock restaurants

--- lib/db.ts
+++ lib/db.ts
@@ -1,6 +1,40 @@
 import {Restaurant} from  "@/types/restaurant"
 
-let restaurants: Restaurant[] = []
+let restaurants: Restaurant[] = [
+    {
+        id: '1',
+        name: 'Pallas',
+        type: 'Mexican',
+        image: 'https://images.unsplash.com/photo-1517836357463-d25dfeac3438?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80',
+        location: '123 Main St',
+        priceRange: '$$',
+        rating: 4.5,
+        reviews: 120,
+        description: 'A delicious Mexican restaurant with a cozy atmosphere.'
+    },
+    {
+        id: '2',
+        name: 'Pallas',
+        type: 'Mexican',
+        image: 'https://images.unsplash.com/photo-1517836357463-d25dfeac3438?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80',
+        location: '123 Main St',
+        priceRange: '$$',
+        rating: 4.5,
+        reviews: 120,
+        description: 'A delicious Mexican restaurant with a cozy atmosphere.'
+    },
+    {
+        id: '3',
+        name: 'Pallas',
+        type: 'Mexican',
+        image: 'https://images.unsplash.com/photo-1517836357463-d25dfeac3438?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80',
+        location: '123 Main St',
+        priceRange: '$$',
+        rating: 4.5,
+        reviews: 120,
+        description: 'A delicious Mexican restaurant with a cozy atmosphere.'
+    }
+]
 
 
 export function getRestaurants() {
