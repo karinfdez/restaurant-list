83358e5c9b5bad4899907f1533e59310e4ceb100
karinfdez <karinfdez@yahoo.com>
2025-12-02 23:33:47 -0800

    Add delete validation

--- app/apis/restaurants/[id]/route.ts
+++ app/apis/restaurants/[id]/route.ts
@@ -1,6 +1,39 @@
 import { updateRestaurant, deleteRestaurant } from "@/lib/db"
+import {restaurantSchema} from "@/lib/schemas/restaurant"
+import {NextResponse} from "next/server"
 
 export async function PATCH(request: Request, { params }: { params: { id: string } }) {
     const body = await request.json()
-    return updateRestaurant(params.id, body)
+    const validatedData = restaurantSchema.safeParse(body)
+    if(!validatedData.success) {
+        return NextResponse.json(
+            { error: validatedData.error.flatten().fieldErrors },
+            { status: 400 }
+        )
+    }
+    return updateRestaurant(params.id, validatedData.data)
+}
+
+
+export async function DELETE(request: Request, { params }: { params: { id: string } }) {
+
+    try {
+        const {id} =params
+
+        if(!id) {
+            return NextResponse.json(
+                {error: "Invalid id"}, 
+                {status: 400}
+            )
+        }
+        const deletedRestaurant = deleteRestaurant(id)
+        
+        if(!deletedRestaurant) {
+            return NextResponse.json({error: "Restaurant not found"}, {status: 404})
+        }
+        return NextResponse.json({success: true})
+
+    }catch(error) {
+        return NextResponse.json({error: "Failed to delete restaurant"}, {status: 500})
+    }
 }
